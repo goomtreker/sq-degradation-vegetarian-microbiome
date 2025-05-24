1ab5909a108627ce5295b81ad18a2ccc7ca689f2
goomtreker <anurovartemiy@gmail.com>
2025-05-25 01:17:52 +0300

    Adjust jkhmmer and diamond

--- code/bin/diamond.sh
+++ code/bin/diamond.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 diamond makedb --in uhgp-100.faa  --db uhgp-100
 
-diamond blastp --masking none --sensitive -f 6 --db data/uhgp-100.dmnd --query data/SQ_degradation.fasta --out SQD_uhgp100.tsv --threads 24
+diamond blastp --masking none --sensitive -f 6 --db data/uhgp-100.dmnd --query data/SQ_degradation_enzymes.fasta --out SQD_uhgp100.tsv --threads 24
