024be145e455c266b02b9c3100de3d3a23c29677
goomtreker <anurovartemiy@gmail.com>
2025-05-25 00:17:15 +0300

    Add enzyme metadata

--- code/bin/diamond.sh
+++ code/bin/diamond.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 diamond makedb --in uhgp-100.faa  --db uhgp-100
 
-diamond blastp --masking none --sensitive -f 6 --db /home/aanurov/beegfs/SQ_project/uhgp-100.dmnd --query ../SQ_degradation.fasta --out SQD_uhgp100.tsv --threads 24
+diamond blastp --masking none --sensitive -f 6 --db data/uhgp-100.dmnd --query data/SQ_degradation.fasta --out SQD_uhgp100.tsv --threads 24
--- code/bin/jackhmmer.sh
+++ code/bin/jackhmmer.sh
@@ -1,1 +1,2 @@
-jackhmmer --cpu 24 -N 8  --tblout sequence_result.tsv --domtblout sequence_domain.tsv ../SQ_degradation.fasta ../uhgp-1$
+#!/bin/bash
+jackhmmer --cpu 24 -N 8  --tblout sequence_result.tsv --domtblout sequence_domain.tsv data/SQ_degradation.fasta data/uhgp-100.fasta
