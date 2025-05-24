d1fc24164eb183c8e1c92114b2ffd6057158429f
goomtreker <anurovartemiy@gmail.com>
2025-05-25 01:10:13 +0300

    Update bin scripts

--- code/bin/jackhmmer.sh
+++ code/bin/jackhmmer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-jackhmmer --cpu 24 -N 8  --tblout sequence_result.tsv --domtblout sequence_domain.tsv data/SQ_degradation.fasta data/uhgp-100.fasta
+jackhmmer --cpu 24 -N 8  --tblout data/sequence_result.tsv --domtblout data/sequence_domain.tsv data/SQ_degradation.fasta data/uhgp-100.fasta
